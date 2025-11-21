e8abc1bdc423eb9f5b653a90bb722a5be551eb45
2025-0235-lgtm <2025-0235@unad.edu.do>
2025-11-20 20:51:42 -0400

    Add files via upload

---
+++ edad.py
@@ -0,0 +1,6 @@
+anio_nacimiento = int(input("Ingresa tu año de nacimiento: "))
+edad = 2025 - anio_nacimiento
+if edad >= 18:
+     print ( "Eres mayor de edad " )
+else:
+     print ("Eres menor de edad " )
